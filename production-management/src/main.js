c85502fe5e1eda4de93fb3cf2286d4ca31746e00
Pavel <podoprigoraps@gmail.com>
2024-04-19 22:04:30 +0400

    удален ненужный mixins

--- production-management/src/main.js
+++ production-management/src/main.js
@@ -2,10 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import globalMethodsMixin from './mixins/globalMethods'
 
 const app = createApp(App)
-
-app.mixin(globalMethodsMixin)
 
 app.use(store).use(router).mount('#app')
